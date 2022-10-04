8b5fa78d14c03f432415abb86f5de5f7893b102a
Louie <dustyfingers01257@gmail.com>
2022-10-04 00:40:16 -0600

    learned more about marcos and how they differ from functions in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mars_weight = calculate_weight_on_mars(175.0);
+    println!("Weight on Mars: {:?}lb", mars_weight);
+}
+
+fn calculate_weight_on_mars(earth_weight: f32) -> f32 {
+    (earth_weight/9.81) * 3.711
 }
