0d44a09e93fc6edbb2e311008b13230dfd8049bd
Louie <dustyfingers01257@gmail.com>
2022-10-04 19:20:15 -0600

    completed mars calculator, learned more about ownership and borrowing in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::io::{self, Read};
+
 fn main() {
-    let mars_weight = calculate_weight_on_mars(175.0);
+    println!("Enter your weight in lbs:");
+
+    // input string is mutably borrowed here 
+    // then its value is changed by the read_line function
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    
+    // any function that returns a Result can be 'unwrapped'
+    // Results contain an Ok or an Err
+    // if an Ok, unwrap() yields the returned value
+    // if an Err, unwrap() halts execution of the program
+    let earth_weight: f32 = input.trim().parse().unwrap();
+
+    let mars_weight = calculate_weight_on_mars(earth_weight);
     println!("Weight on Mars: {:?}lb", mars_weight);
 }
 
